8fdb3ac1f21d1dda7de06e1da4b41c0f59539219
oahnus <15751774919@163.com>
2016-07-20 21:38:17 +0800

    增加对用户登录的监控，用户登陆时获取其ip地址，并对客户端发起的聊天请求做处理，将目标好友的ip发送回客户端，由客户端与好友建立连接

---
+++ src/top/oahnus/Bean/Message.java
@@ -0,0 +1,46 @@
+package top.oahnus.Bean;
+
+/**
+ * Created by oahnus on 2016/7/20.
+ */
+public class Message {
+    private static final long serialVersionUID = 1234L;
+
+    private String code;
+    private String targetID;
+
+    public String getTargetID() {
+        return targetID;
+    }
+
+    public void setTargetID(String targetID) {
+        this.targetID = targetID;
+    }
+
+    private String content;
+    private String extra;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getExtra() {
+        return extra;
+    }
+
+    public void setExtra(String extra) {
+        this.extra = extra;
+    }
+}
